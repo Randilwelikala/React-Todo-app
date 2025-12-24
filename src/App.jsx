eb7d58a4b00a6b1953a6c9605db4aced72af0009
randilwelikala <randilgimantha646@gmail.com>
2025-12-25 00:05:02 +0530

    changed app.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -22,42 +22,7 @@ function App(){
   
   return (
     <div>
-      {/* <div>
-        <h1>Todo App</h1>
-      </div>   
-        <Button text="Click Me" onClicked={handleClick}></Button> 
-
-        <Card
-          title="Toy"
-          description="Remote control car"
-          footer="Rs. 3000.00"
-        />
-
-        <Card
-          title="Book"
-          description="Stories"
-          footer="Rs. 275.00"
-        />
-
-        <Card
-          title="Marker Pen"
-          description="Blue color"
-          footer="Rs. 190.00"
-        />
-
-
-        <Input lable="Enter your name" placeholder="Type here ..."></Input>
-        <Input lable="Enter your email" placeholder="example@gmail.com"></Input>
-
-
-        <Counter></Counter>
-
-        
-
-        <Modal></Modal>
-        <br></br>
-
-        <Tabs></Tabs> */}
+      
         <Sidebar></Sidebar>
         <Navbar items={["Home", "About", "Contact"]} />
 
